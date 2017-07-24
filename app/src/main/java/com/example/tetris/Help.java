88ceb2ebd29121ec7f1b4d81f1c87cd9f18e9ba3
czw <c1584796568@gmail.com>
2017-07-24 16:32:22 +0800

    去除帮助界面标题栏，更改帮助界面背景图片

--- app/src/main/java/com/example/tetris/Help.java
+++ app/src/main/java/com/example/tetris/Help.java
@@ -3,22 +3,21 @@ package com.example.tetris;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
-/**
- * Created by Administrator on 2017/7/22.
- */
 
 public class Help extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.help);
+//        ActionBar actionBar = getSupportActionBar();
+//        if(actionBar != null)
+//            actionBar.hide();
         Button help_back = (Button)findViewById(R.id.help_back);
-        TextView help_text = (TextView)findViewById(R.id.help_text);
         help_back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -26,8 +25,5 @@ public class Help extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        String text = "俄罗斯方块是一个休闲游戏：" +
-                "\n"+"左右移动、旋转以及加速下落";
-        help_text.setText(text);
     }
 }
